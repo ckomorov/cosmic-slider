909c63e01a72776cf8dc771b83d4c674121c806a
Sergey <ckomorov1@gmail.com>
2019-08-05 16:40:23 +0300

    first slide

--- src/js/script.js
+++ src/js/script.js
@@ -5,7 +5,8 @@ function initPageSlider() {
 
 	$slider.slick({
 		arrows: false,
-		vertical: true
+		vertical: true,
+		dots: true
 	});
 
 	$slider.mousewheel(function(e){
@@ -27,11 +28,41 @@ function initPageSlider() {
 			}
 		}, threshold: 50 
 	});
+
+	chooseTextOnFirstSlide();
+	
+}
+
+
+function chooseTextOnFirstSlide() {
+
+	const $ps_top_1 = $('.page-slide-1-top');
+
+	$('#tw-1').on('mouseover', function(){
+		$ps_top_1.addClass('left');
+		$ps_top_1.removeClass('middle', 'right');
+	});
+
+	$('#tw-2').on('mouseover', function(){
+		$ps_top_1.addClass('middle');
+		$ps_top_1.removeClass('right', 'left');
+	});
+
+	$('#tw-3').on('mouseover', function(){
+		$ps_top_1.addClass('right');
+		$ps_top_1.removeClass('left', 'middle');
+	});
 }
 
 
 
-
 $('document').ready(function() {
+
 	initPageSlider();
+
+	
+	
+
+
+	
 });
